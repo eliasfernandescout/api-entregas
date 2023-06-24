479b29e2fa33acc0c0eda0ac6c5c570f44c1e83b
eliasfernandescout <eliasfernandescout@gmail.com>
2023-06-24 20:18:15 -0300

    first commit

---
+++ src/main/java/com/devhub/apientregas/ApiEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.devhub.apientregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEntregasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEntregasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devhub/apientregas/ApiEntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.apientregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEntregasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
