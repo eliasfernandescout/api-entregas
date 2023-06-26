51b7a477a220921cbf8219e80131ae5418bdf02c
eliasfernandescout <eliasfernandescout@gmail.com>
2023-06-26 00:37:34 -0300

    update project

--- src/main/java/com/apientregas/ApiController.java
+++ src/main/java/com/apientregas/ApiController.java
@@ -1,4 +1,4 @@
-package com.devhub.apientregas;
+package com.apientregas;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/com/apientregas/ApiEntregasApplicationTests.java
+++ src/test/java/com/apientregas/ApiEntregasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devhub.apientregas;
+package com.apientregas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
