75c8aa14c31f27ebbffbd5baa94817577214d31a
Elias Fernandes <eliasfernandescout@gmail.com>
2023-09-19 21:23:11 -0300

    update project

--- src/main/java/com/apientregas/service/AtivacaoClienteService.java
+++ src/main/java/com/apientregas/service/AtivacaoClienteService.java
@@ -3,12 +3,14 @@ package com.apientregas.service;
 import com.apientregas.modelo.Cliente;
 import com.apientregas.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired
+
+    @Autowired(required = false)
     private Notificador notificador;
 
     public void ativar(Cliente cliente) {
