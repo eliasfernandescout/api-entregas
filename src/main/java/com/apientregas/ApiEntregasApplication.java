fcef0fa7f8a47c7354cf76b85861684b5d448c3c
Elias Fernandes <eliasfernandescout@gmail.com>
2023-09-20 14:57:04 -0300

    feat: adicionada conexao com o banco mysql e configuracoes no application.properties

--- src/main/java/com/apientregas/ApiEntregasApplication.java
+++ src/main/java/com/apientregas/ApiEntregasApplication.java
@@ -1,14 +1,17 @@
 package com.apientregas;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@ComponentScan({"com.apientregas"})
 //@EntityScan("com.apientregas")
 //@EnableJpaRepositories("com.delivery.repository")
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ApiEntregasApplication {
 
     public static void main(String[] args) {
