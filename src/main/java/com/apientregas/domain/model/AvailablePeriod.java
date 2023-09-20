6c8123c353a33dfbef9a2a23464ed6bdb42502d2
Elias Fernandes <eliasfernandescout@gmail.com>
2023-09-20 15:06:23 -0300

    feat: add AvailablePeriod model

---
+++ src/main/java/com/apientregas/domain/model/AvailablePeriod.java
@@ -0,0 +1,20 @@
+package com.apientregas.domain.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "merchant")
+public class AvailablePeriod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "day")
+    private String day;
+    @Column(name = "start_time")
+    private String starTime;
+    @Column(name = "end_time")
+    private String endTime;
+    @Column(name = "available_holiday")
+    private String isAvailableHoliday;
+
+}
